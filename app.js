5dfeaf8817f0413326356786135faacc47a4837f
MathieuJ <MathieuJ@users.noreply.github.com>
2016-01-13 14:41:10 +0100

    u

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-var app = angular.module('plunker', []);
+var app = angular.module('colons', []);
 
 app.controller('MainCtrl', function($scope) {
   $scope.name = 'World';
+  $scope.c1 = new Colon("robert", 3);
+  $scope.c2 = new Colon("roberta", 5);
 });
+
+app.module('colons').factory('BatimentProto', function() {
+  var BatimentProto = function(nom, type, materiaux, place, durabilite, etancheite, isolation, confort, beaute, autre) {
+    this.nom = nom;
+    this.materiaux = materiaux;
+    this.durabilite = durabilite;
+    this.etancheite = etancheite;
+    this.isolation = isolation;
+    this.confort = confort;
+    this.place = place;
+    this.type = type;
+    this.autre = autre;
+  };
+  return BatimentProto;
+})
+ .factory('Colon', function() {
+  var Colon = function(nom, age) {
+    this.nom = nom;
+    this.age = age;
+  };
+  Colon.prototype.getAge = function(date) {
+    return 3;
+  };
+  return Colon;
+})
+;
+
+
