389d97f3fa787c1a0bd23bb5403ceccae41e7a45
Myself J <yo@yo.com>
2016-01-28 08:22:33 +0100

    steeve

---
+++ steeve/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('accueilCtrl', function($scope) {
+
+})
+   
+.controller('mesJeuxCtrl', function($scope) {
+
+})
+   
+.controller('mesAmisCtrl', function($scope) {
+
+})
+      
+.controller('ficheJeuCtrl', function($scope) {
+
+})
+   
+.controller('ficheAmiCtrl', function($scope) {
+
+})
+   
+.controller('pretsDeJeuxCtrl', function($scope) {
+
+})
+   
+.controller('partiesCtrl', function($scope) {
+
+})
+   
+.controller('partieCtrl', function($scope) {
+
+})
+   
+.controller('outilsCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('parametresCtrl', function($scope) {
+
+})
+   
+.controller('rechercheDeJeuxCtrl', function($scope) {
+
+})
+ 
