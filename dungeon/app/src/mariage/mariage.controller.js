a467c3f4d5711c482850105b03477e426c39a912
MathieuJ <MathieuJ@users.noreply.github.com>
2016-03-22 16:48:10 +0100

    Update mariage.controller.js

--- dungeon/app/src/mariage/mariage.controller.js
+++ dungeon/app/src/mariage/mariage.controller.js
@@ -92,8 +92,9 @@
 
     self.mariage = MariageService.get();
     
-    self.ajouteInvite = function() {
-      MariageService.addInvite(new Personne('coco', 1, 'M'));
+    self.ajoute = function() {
+      Personne p = new Personne(self.nom, 1, 'M');
+      MariageService.addInvite(p);
     }
   }
 })();
