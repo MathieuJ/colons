c10018501cf60e343b30cb5802ada5f929356de7
MathieuJ <MathieuJ@users.noreply.github.com>
2016-03-29 10:30:46 +0200

    Update mariage.service.js

--- dungeon/app/src/mariage/mariage.service.js
+++ dungeon/app/src/mariage/mariage.service.js
@@ -5,29 +5,38 @@
     .service('MariageService', ['Personne', MariageService]);
 
   function MariageService(Personne){
-    var Mariage = {
-      'marie1' : new Personne('Marié', 1, 'M'),
-      'marie2' : new Personne('Mariée', 2, 'M'),
-      invites : [],
-      eglise : { actif : false },
-      mairie : { actif : false },
-      enterrementMarie1 : { actif : false },
-      enterrementMarie2 : { actif : false },
-      soiree : { actif : false },
-      vinHonneur : { actif : false },
-      lendemain : { actif : false }
-    };
+    var Mariage ;
 
     // Promise-based API
     this.init = function() {
-      console.log("init recup de localstorage");
+      Mariage = {
+        'marie1' : new Personne('Marié', 1, 'M'),
+        'marie2' : new Personne('Mariée', 2, 'M'),
+        invites : [],
+        eglise : { actif : false },
+        mairie : { actif : false },
+        enterrementMarie1 : { actif : false },
+        enterrementMarie2 : { actif : false },
+        soiree : { actif : false },
+        vinHonneur : { actif : false },
+        lendemain : { actif : false }
+      }
     };
       
     this.addInvite = function(personne) {
       Mariage.invites.push(personne);
     };
     
+    this.save = function() {
+      localStorageService.set('mariage', Mariage);
+    }
+    
     this.get = function() {
+      if (!!Mariage) {
+        this.init();
+      } else {
+        Mariage = localStorageService.get('mariage');
+      }
       return Mariage;
     }
   }
