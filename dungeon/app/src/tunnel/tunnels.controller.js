1200e6de0b5e02e1502fdc0c83ee316c8d22f11e
MathieuJ <MathieuJ@users.noreply.github.com>
2016-04-15 15:36:43 +0200

    Update tunnels.controller.js

--- dungeon/app/src/tunnel/tunnels.controller.js
+++ dungeon/app/src/tunnel/tunnels.controller.js
@@ -15,11 +15,39 @@
    * @constructor
    */
   function TunnelsController(PartieService, $timeout) {
-    var self = this;
+    var vm = this;
 
-    self.users = [];
-
-    PartieService.init();
-
+    vm.users = [];
+    
+    //PartieService.init();
+    
+  vm.map=[];
+  var taille=32;
+  for (var i =0; i < taille; i++)   {
+   var ligne = [];
+   for (var j =0;  j < taille; j++){
+     ligne.push({i: i, j: j});
+ }
+   vm.map.push(ligne);
   }
+  
+  function get(x, y){
+    return vm.map[x][y];
+  }
+  vm.selectedCase;
+  vm.selectedUnite;
+  vm.select = function(x, y){
+    var macase=get(x, y);
+    vm.selectedCase=macase;
+    if (vm.selectedUnite){
+      vm.selectedUnite = false;
+    } else {
+       var unite = null; //getUnite(x, y);
+       if (unite){
+         vm.selectedUnite= unite;
+         //vm.destinations=calculeChemins();
+       }
+    }
+  }
+
 })();
