bab1bb583ff70e10879cbd9d98b65b7ea7030dde
MathieuJ <MathieuJ@users.noreply.github.com>
2016-04-15 15:46:33 +0200

    Update tunnels.controller.js

--- dungeon/app/src/tunnel/tunnels.controller.js
+++ dungeon/app/src/tunnel/tunnels.controller.js
@@ -3,7 +3,7 @@
     angular
         .module('tunnels')
         .controller('TunnelsController', [
-            'partieService', '$timeout',
+            '$timeout',
             TunnelsController
         ]);
 
@@ -14,7 +14,7 @@
      * @param avatarsService
      * @constructor
      */
-    function TunnelsController(PartieService, $timeout) {
+    function TunnelsController($timeout) {
         var vm = this;
 
         vm.users = [];
@@ -23,25 +23,60 @@
 
         vm.map = [];
         var taille = 32;
-        for (var i = 0; i < taille; i++) {
+        for (var y = 0; y < taille; y++) {
             var ligne = [];
-            for (var j = 0; j < taille; j++) {
+            for (var x = 0; x < taille; x++) {
                 ligne.push({
-                    i: i,
-                    j: j
+                    x: x,
+                    y: y,
+                    type: "terre"
                 });
             }
             vm.map.push(ligne);
         }
 
         function get(x, y) {
-            return vm.map[x][y];
+            if (x >= 0 && y >= 0 && x < taille && y < taille) {
+                return vm.map[x][y];
+            }
+            return null;
         }
-        vm.selectedCase;
+        
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        (function() {
+            var x = 5; y= 5;
+            for (var i = 0; i < taille; i++) {
+                var cell = get(x,y);
+                if (cell) {
+                    cell.type = "eau";
+                    switch (getRandomInt(0, 1)) {
+                        case 0:
+                            if (x < taille - 1) x += 1;
+                            break;
+                        case 1:
+                            if (x > 0) x -= 1;
+                            break;
+                        case 2:
+                            if (y < taille - 1) y += 1;
+                            break;
+                        case 3:
+                            if (y > 0) y -= 1;
+                            break;
+                        }
+                    }
+                }
+            }    
+        })();
+        
+        
+        vm.selectedCell;
         vm.selectedUnite;
         vm.select = function(x, y) {
-            var macase = get(x, y);
-            vm.selectedCase = macase;
+            var macell = get(x, y);
+            vm.selectedCell = macell;
             if (vm.selectedUnite) {
                 vm.selectedUnite = false;
             } else {
