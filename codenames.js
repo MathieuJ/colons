e3e5aeff960c3f1121c07d04f954233f058c544a
MathieuJ <MathieuJ@users.noreply.github.com>
2017-10-13 19:12:10 +0200

    Update codenames.js

--- codenames.js
+++ codenames.js
@@ -46,6 +46,8 @@ app.controller('CodenamesCtrl', function($scope, $location) {
         $scope.tableauIndices = getIndices();
         $scope.graineEspion = Math.floor(Math.random() * 1000000);
         $scope.generateRandomFonction($scope.graineEspion);
+        console.log($scope.graineEspion);
+        console.log(Math.random());
         $scope.tableauMots = getMots($scope.dico);
         $scope.demarree = true;
         console.log("resultat : ", $scope.tableauIndices, $scope.tableauMots);
@@ -64,6 +66,8 @@ app.controller('CodenamesCtrl', function($scope, $location) {
     $scope.generateEspion = function() {
         console.log($scope.graineEspion);
         $scope.generateRandomFonction($scope.graineEspion);
+        console.log($scope.graineEspion);
+        console.log(Math.random());
         
         $scope.tableauMots = getMots($scope.dico);
         $scope.demarree = true;
